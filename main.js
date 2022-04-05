ea2a870dc3dfbfa86acebdb5869897576d4a1dda
Shriy1556k <81312355+Shriy1556k@users.noreply.github.com>
2022-04-05 20:00:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+timer = 0;
+timerCheck = "";
+drawn_sketch="";
+Answer_holder="";
+score=0;
+Quickdraw=["apple","Bus","door","house"];
+
+
+function preload()
+{
+    Classifer = ml5.imageClassifier("DoodleNet");
+}
+
+function setup()
+{
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+function clearCanvas()
+{
+    background("white")
+}
+function draw()
+{
+    strokeWeight(5);
+    stroke(0);
+    if (mouseIsPressed) 
+    {
+    line(pmouseX,pmouseY,mouseX,mouseY);    
+    }
+}
+
+function classifyCanvas()
+{
+    Classifer.classify(canvas,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if (error) 
+    {
+    console.error(error);        
+    }
+    console.log(results);
+
+    document.getElementById("label").innerHTML = "label : " + results[0].label ;
+    document.getElementById("confidence").innerHTML="Confidence : " + Math.round(results[0].confidence*100)+"%";
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
